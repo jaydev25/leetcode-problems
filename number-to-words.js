9859984c031d45b190403ceebb536994a7f0cbd9
jaydev25 <42494740+jaydev25@users.noreply.github.com>
2022-06-12 14:17:12 +0530

    Latest soln with fixes

--- number-to-words.js
+++ number-to-words.js
@@ -5,7 +5,7 @@
 var numberToWords = function(num) {
     
     var key = [
-        ['' ,'One','Two','Three','Four','Five','Six','Seven','Eight','Nine'],
+        ['Zero' ,'One','Two','Three','Four','Five','Six','Seven','Eight','Nine'],
         ['', 'Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'],
         ['Hundred'],
         ['Thousand'],   
@@ -23,10 +23,10 @@ var numberToWords = function(num) {
         'Ten Two': 'Twelve',
         'Ten Three': 'Thirteen',
         'One Three': 'Thirteen',
-        'Ten Four': 'Forteen',
-        'One Four': 'Forteen',
-        'Ten Five': 'Fiveteen',
-        'One Five': 'fiveteen',
+        'Ten Four': 'Fourteen',
+        'One Four': 'Fourteen',
+        'Ten Five': 'Fifteen',
+        'One Five': 'fifteen',
         'Ten Six': 'Sixteen',
         'One Six': 'Sixteen',
         'Ten Seven': 'Seventeen',
@@ -35,9 +35,11 @@ var numberToWords = function(num) {
         'One Eight': 'Eighteen',
         'Ten Nine': 'Nineteen',
         'One Nine': 'Nineteen',
+       
+
     }
     
-    var n = String(+num).split(""), pos = 0;
+    var n = String(+num).split(""), pos = 0, zerosAdded = 0;
     
     while(n.length > 0) {
         var temp = n.pop()
@@ -45,29 +47,46 @@ var numberToWords = function(num) {
         if (pos > 1) {
             if (key[pos] && key[pos][0]) {
                 number.unshift(`${key[0][+temp]} ${key[pos][0]}`);
+                if (temp == 0) {
+                    zerosAdded ++;
+                }
             } else {
                 if (pos === 8) {
                     number.unshift(`${key[0][+temp]} ${key[2][0]}`);
+                    if (temp == 0) {
+                        zerosAdded ++;
+                    }
                 } else if (pos === 9) {
                     number.unshift(`${key[0][+temp]} ${key[6][1]}`);
+                     if (temp == 0) {
+                        zerosAdded ++;
+                    }
                 } else {
+                    console.log(pos, number.length, n.length)
                     number.unshift(`${key[1][+temp]}`);
+                    if ((number[0].length > 0 || n.length === 1) && number[1].includes('Zero')) {
+                        number[1] = number[1].split(' ')[1];
+                    }
                 }
             }
-        } else {
+        } else if (key[pos][+temp] !== 'Zero') {
             number.unshift(key[pos][+temp]);
         }
         pos ++;
     }
-            
+    
+    console.log(number)
+    
+    number = number.filter(n => n.length > 0 && !n.includes('Zero'))
+                
     var answer = number.join(' ');
     
     for (var rep in replacements) {
         if (answer.includes(rep)) {
-            answer = answer.replace(rep, replacements[rep])
+            answer = answer.replaceAll(rep, replacements[rep])
         }
     }
         
-    return answer
+    return answer.trim() || 'Zero'
     
 };
